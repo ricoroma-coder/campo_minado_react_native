aeedd264fed6777ac450db6221138bc55efcb916
Henrique Roma <roma.fulldev@gmail.com>
2021-05-24 20:24:34 -0300

    Tratando estilos do componente Field.js

--- src/components/Field.js
+++ src/components/Field.js
@@ -2,13 +2,24 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import params from '../params'
 
-export default props => {
+export default ({ mined, opened, nearMines }) => {
     const styleField = [styles.field]
+    if (opened) styleField.push(styles.opened)
     if (styleField.length === 1) styleField.push(styles.regular)
+
+    let color = null
+    if (nearMines > 0) {
+        if (nearMines == 1) color = '#2A2BD7'
+        if (nearMines == 2) color = '#2B520F'
+        if (nearMines > 2 && nearMines < 6) color = '#F9060A'
+        if (nearMines >= 6) color = '#F221A9'
+    }
 
     return (
         <View style={styleField}>
-
+            {!mined && opened && nearMines > 0
+            ? <Text style={[styles.label, {color: color}]}> {nearMines}</Text>
+            : false}
         </View>
     )
 }
@@ -25,5 +36,15 @@ const styles = StyleSheet.create({
         borderTopColor: '#CCC',
         borderRightColor: '#333',
         borderBottomColor: '#333',
+    },
+    opened: {
+        backgroundColor: '#999',
+        borderColor: '#777',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    label: {
+        fontWeight: 'bold',
+        fontSize: params.fontSize,
     }
 })
