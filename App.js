59af62d7568fbd1063c58237083002d9f84ef533
Henrique Roma <roma.fulldev@gmail.com>
2021-05-27 18:36:59 -0300

    Criando estilos de Flags maiores

--- App.js
+++ App.js
@@ -7,30 +7,25 @@ import Field from './src/components/Field'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Iniciando o mines!</Text>
-      <Text style={styles.instructions}>
-        Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
-       </Text>
-
-       <Field />
-       <Field opened />
-       <Field opened nearMines={1} />
-       <Field opened nearMines={2} />
-       <Field opened nearMines={3} />
-       <Field opened nearMines={6} />
-       <Field opened mined />
-       <Field opened mined exploded />
-       <Field flagged />
-       <Field flagged opened />
+		<Field />
+		<Field opened />
+		<Field opened nearMines={1} />
+		<Field opened nearMines={2} />
+		<Field opened nearMines={3} />
+		<Field opened nearMines={6} />
+		<Field opened mined />
+		<Field opened mined exploded />
+		<Field flagged />
+		<Field flagged opened />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
